25fbcac530627cba57071db23c7dcf3dab646101
Joao Lara <joaovdelara@gmail.com>
2025-02-03 12:27:06 -0300

    create client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,10 @@
-import { useEffect, useState } from 'react'
-import { Wallet } from '@mercadopago/sdk-react'
+import React from 'react'
 
-function App() {
-  const [preferenceId, setPreferenceId] = useState(null)
-
-  // Função para criar a preferência e pegar o preference_id
-  const createPreference = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/payment/create_preference', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          items: [
-            {
-              title: 'Produto Exemplo',
-              quantity: 1,
-              unit_price: 100,
-            },
-          ],
-        }),
-      })
-
-      const data = await response.json()
-      setPreferenceId(data.id)
-    } catch (error) {
-      console.error('Erro ao criar preferência:', error)
-    }
-  }
-
-  useEffect(() => {
-    createPreference()
-  }, [])
-
-  if (!preferenceId) {
-    return <div>Carregando...</div>
-  }
-
+const App = () => {
   return (
-    <div>
-      <h1>Pagamento Mercado Pago</h1>
-      <Wallet preferenceId={preferenceId} />
-    </div>
+    <main>
+        app
+    </main>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { initMercadoPago } from '@mercadopago/sdk-react'
-initMercadoPago('TEST-60606327-601b-44af-8740-dd77ef0bd030');
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
